b70908508dfb4054f369d0e4f236124f448e8a04
Simon Faustmann <FaustSim@yahoo.com>
2020-08-28 17:09:54 +0200

    Now calculating X, Y NNN, NN and N just once in every loop

--- source/Chip8.d
+++ source/Chip8.d
@@ -103,7 +103,7 @@ class Chip8 {
           }
 
         // Just for debuging
-        writeln("\nSTACK | PC | OPCODE");
+        debug writeln("\nSTACK | PC | OPCODE");
     }
 
     // Main emulation loop
@@ -130,11 +130,18 @@ class Chip8 {
             //     this.key = -1;
             // }
 
-            writef("\r%s | %s | 0x%X", this.stack, this.pc, this.opcode & 0xFFFF);
+            debug writef("\r%s | %s | 0x%X", this.stack, this.pc, this.opcode & 0xFFFF);
             //writeln(pressedKey);
 
             //DrawText(cast(char*)format("0x%X", this.opcode), 25, 25, 20, Colors.RAYWHITE);
             //DrawText(cast(char*)format("%s", this.pc), 25, 45, 20, Colors.RAYWHITE);
+
+            // Calculation the X, Y, NNN, NN and N values
+            immutable auto x = (this.opcode & 0x0F00) >> 8;
+            immutable auto y = (this.opcode & 0x00F0) >> 4;
+            immutable auto nnn = (this.opcode & 0x0FFF);
+            immutable auto nn = (this.opcode & 0x00FF);
+            immutable auto n = (this.opcode & 0x000F);
 
             // Decode opcodes
             // The & just keeps the bit at F and sets the rest to 000 so we can decode it
@@ -169,13 +176,13 @@ class Chip8 {
                 }
                 // 0x1NNN jumps to address NNN
                 case 0x1000: {
-                    this.pc = this.opcode & 0x0FFF;
+                    this.pc = cast(short)nnn;
                     break;
                 }
 
                 // 0x2NNN calls a subroutine at address NNN -> we need to keep track of the pc in the stack because of the return
                 case 0x2000: {
-                    //writefln("Call: %s", this.opcode & 0x0FFF);
+                    //writefln("Call: %s", nnn);
                     //this.stack[this.sp++] = this.pc;
                     this.pc += 2;
                     this.stack[this.sp] = this.pc;
@@ -186,7 +193,7 @@ class Chip8 {
 
                 //0x3XNN skips the next instruction when V[X] equals NN
                 case 0x3000: {
-                    if(this.V[(this.opcode & 0x0F00) >> 8] == (this.opcode & 0x00FF)) {
+                    if(this.V[x] == (nn)) {
                         this.pc += 4;
                     } else {
                         this.pc += 2;
@@ -196,7 +203,7 @@ class Chip8 {
 
                 // 0x4XNN skips the next instruction when V[X] doesn't euqal NN
                 case 0x4000: {
-                    if(this.V[(this.opcode & 0x0F00) >> 8] != (this.opcode & 0x00FF)) {
+                    if(this.V[x] != (nn)) {
                         this.pc += 4;
                     } else {
                         this.pc += 2;
@@ -206,7 +213,7 @@ class Chip8 {
 
                 // 0x5XY0 skips the next instruction when V[X] equals V[Y]
                 case 0x5000: {
-                    if(this.V[(this.opcode & 0x0F00) >> 8] == (this.opcode & 0x00F0) >> 4) {
+                    if(this.V[x] == y) {
                         this.pc += 4;
                     } else {
                         this.pc += 2;
@@ -216,14 +223,14 @@ class Chip8 {
                 
                 // 0x6XNN sets V[X] to NN
                 case 0x6000: {
-                    this.V[(this.opcode & 0x0F00) >> 8] = (this.opcode & 0x00FF);
+                    this.V[x] = cast(char)nn;
                     this.pc += 2;
                     break;
                 }
                 
                 // 0x7XNN adds NN to V[X] (wihout changing the carry flag)
                 case 0x7000: {
-                    this.V[(this.opcode & 0x0F00) >> 8] += (this.opcode & 0x00FF);
+                    this.V[x] += (nn);
                     this.pc += 2;
                     break;
                 }
@@ -233,72 +240,72 @@ class Chip8 {
                     switch (this.opcode & 0x000F) {
                         // 0x8XY0 sets V[X] to V[Y]
                         case 0x0000: {
-                            this.V[(this.opcode & 0x0F00) >> 8] = this.V[(this.opcode & 0x00F0) >> 4];
+                            this.V[x] = this.V[y];
                             this.pc += 2;
                             break;
                         }
 
                         // 0x8XY1 sets V[X] to V[X] OR V[Y]
                         case 0x0001: {
-                            this.V[(this.opcode & 0x0F00) >> 8] |= this.V[(this.opcode & 0x00F0) >> 4];
+                            this.V[x] |= this.V[y];
                             this.pc += 2;
                             break;
                         }
 
                         // 0x8XY2 sets V[X] to V[X] AND V[Y]
                         case 0x0002: {
-                            this.V[(this.opcode & 0x0F00) >> 8] &= this.V[(this.opcode & 0x00F0) >> 4];
+                            this.V[x] &= this.V[y];
                             this.pc += 2;
                             break;
                         }
 
                         // 0x8XY3 sets V[X] to V[X] XOR V[Y]
                         case 0x0003: {
-                            this.V[(this.opcode & 0x0F00) >> 8] ^= this.V[(this.opcode & 0x00F0) >> 4];
+                            this.V[x] ^= this.V[y];
                             this.pc += 2;
                             break;
                         }
 
                         // 0x8XY4 adds V[Y] to V[X]. V[F] is set to 1 when there's a carry, if not to 0.
                         case 0x0004: {
-                            if(this.V[(this.opcode & 0x00F0) >> 4] > (0xFF - this.V[(this.opcode & 0x0F00) >> 8])) {
+                            if(this.V[y] > (0xFF - this.V[x])) {
                                 this.V[0xF] = 1; //setting the carry
                             } else {
                                 this.V[0xF] = 0;
                             }
-                            this.V[(this.opcode & 0x0F00) >> 8] += this.V[(this.opcode & 0x00F0) >> 4];
+                            this.V[x] += this.V[y];
                             this.pc += 2;
                             break;
                         }
 
                         // 0x8XY5 subtracts V[Y] from V[X]. V[F] is set to 1 when there's a borrow, if not to 0.
                         case 0x0005: {
-                            if(this.V[(this.opcode & 0x0F00) >> 8] > (0xFF - this.V[(this.opcode & 0x00F0) >> 4])) {
+                            if(this.V[x] > (0xFF - this.V[y])) {
                                 this.V[0xF] = 1; //setting the carry
                             } else {
                                 this.V[0xF] = 0;
                             }
-                            this.V[(this.opcode & 0x0F00) >> 8] -= this.V[(this.opcode & 0x00F0) >> 4];
+                            this.V[x] -= this.V[y];
                             this.pc += 2;
                             break;
                         }
 
                         // 0x8XY6 Stores the least significant bit of V[X] in V[F] and then shifts V[X] to the right by 1
                         case 0x0006: {
-                            this.V[0xF] = this.V[(this.opcode & 0x0F00) >> 8] & 0x1;
-                            this.V[(this.opcode & 0x0F00) >> 8] >>= 1 & 0xFF;
+                            this.V[0xF] = this.V[x] & 0x1;
+                            this.V[x] >>= 1 & 0xFF;
                             this.pc += 2;
                             break;
                         }
 
                         // 0x8XY7 sets V[X] to V[Y] - V[X]. V[F] is set to 1 when there's a borrow, if not to 0
                         case 0x0007: {
-                            if(this.V[(this.opcode & 0x0F00) >> 8] > (0xFF - this.V[(this.opcode & 0x00F0) >> 4])) {
+                            if(this.V[x] > (0xFF - this.V[y])) {
                                 this.V[0xF] = 1; //setting the carry
                             } else {
                                 this.V[0xF] = 0;
                             }
-                            this.V[(this.opcode & 0x0F00) >> 8] = this.V[(this.opcode & 0x00F0 >> 4)] -= this.V[(this.opcode & 0x0F00 >> 8)];
+                            this.V[x] = this.V[(this.opcode & 0x00F0 >> 4)] -= this.V[(this.opcode & 0x0F00 >> 8)];
                             this.pc += 2;
                             break;
                         }
@@ -306,7 +313,7 @@ class Chip8 {
                         // 0x8YXE Stores the most significant bit of V[X] in V[F] and then shifts V[X] to the left by 1
                         case 0x000E: {
                             this.V[0xF] = (this.V[(this.opcode & 0xF00) >> 8] & 0x80) >> 7;
-                            this.V[(this.opcode & 0x0F00) >> 8] <<= 1;
+                            this.V[x] <<= 1;
                             this.pc += 2;
                             break;
                         }
@@ -320,7 +327,7 @@ class Chip8 {
 
                 // 0x9XY0 skips next instruction when V[X] doesn't equal V[Y]
                 case 0x9000: {
-                    if(this.V[(this.opcode & 0x0F00) >> 8] != this.V[(this.opcode & 0x00F0) >> 4]) {
+                    if(this.V[x] != this.V[y]) {
                         this.pc += 4;
                     } else {
                         this.pc += 2;
@@ -330,21 +337,21 @@ class Chip8 {
                 
                 // 0xANNN sets I to the Address in NNN
                 case 0xA000: {
-                    this.I = this.opcode & 0x0FFF;
+                    this.I = cast(short)nnn;
                     this.pc += 2;
                     break;
                 }
 
                 // 0xBNNN jumps to the address NNN + V[0]
                 case 0xB000: {
-                    this.pc = (this.opcode & 0x0FFF) + this.V[0];
+                    this.pc = (this.opcode & 0x0FFF) + this.V[0]; // Using variable nnn here gives me an error, I should look into it
                     break;
                 }
 
                 // 0xCX00 sets V[X] to NN AND random between 0 and 255
                 case 0xC000: {
                     immutable int r = uniform(0, 255, rnd);
-                    this.V[(this.opcode & 0x0F00) >> 8] = r & (this.opcode & 0x00FF);
+                    this.V[x] = r & (this.opcode & 0x00FF); // Using variable nn here gives me an error, I should look into it
                     this.pc += 2;
                     break;
                 }
@@ -352,10 +359,10 @@ class Chip8 {
                 // 0xDXYN draws a sprite at (V[X] , V[Y]) ... Well i have to look into this part a bit more
                 case 0xD000: {
                     this.V[0xF] = 0;
-                    for (int line = 0; line < (this.opcode & 0x000F); line++) {
+                    for (int line = 0; line < (n); line++) {
                         for (int bit = 0; bit < 8; bit++) {
                             if ((this.memory[this.I + line] & (0x80 >> bit)) != 0) {
-                                int p = ((this.V[(this.opcode & 0x0F00) >> 8] + bit) + ((this.V[(this.opcode & 0x00F0) >> 4] + line) * 64)) % 2048;
+                                int p = ((this.V[x] + bit) + ((this.V[y] + line) * 64)) % 2048;
                                 this.V[0xF] |= (this.gfx[p] == 1) ? 1 : 0;
                                 this.gfx[p] ^= 1;
                             }
@@ -371,7 +378,7 @@ class Chip8 {
                     switch (this.opcode & 0xF0FF) {
                         // 0xEX9E skips the next instruction when the key saved in V[X] is pressed
                         case 0xE09E: {
-                            if (this.V[(this.opcode & 0x0F00) >> 8] == this.key) {
+                            if (this.V[x] == this.key) {
                                 this.pc += 4;
                             } else {
                                 this.pc += 2;
@@ -382,7 +389,7 @@ class Chip8 {
 
                         // 0xEXA1 skips the next instruction when the key saved in V[X] is not pressed
                         case 0xE0A1: {
-                            if (this.V[(this.opcode & 0x0F00) >> 8] == this.key) {
+                            if (this.V[x] == this.key) {
                                 this.pc += 2;
                             } else {
                                 this.pc += 4;
@@ -401,7 +408,7 @@ class Chip8 {
                     switch(this.opcode & 0x00FF) {
                         // 0xFX07 sets V[X] to the delayTimer
                         case 0x0007: {
-                            this.V[(this.opcode & 0x0F00) >> 8] = this.delayTimer;
+                            this.V[x] = this.delayTimer;
                             this.pc += 2;
                             break;
                         }
@@ -410,7 +417,7 @@ class Chip8 {
                         case 0x000A: {
                             // TODO when implementing keyboard
                             writeln("Keyboard 3");
-                            this.V[(this.opcode & 0x0F00) >> 8] = cast(char)this.key;
+                            this.V[x] = cast(char)this.key;
                             this.pc += 2;
                             break;
                         }
@@ -439,23 +446,23 @@ class Chip8 {
                         //0xFX29 sets I to the location of the sprite for the character in V[X]
                         case 0x0029: {
                             // TODO when implementing gfx
-                            this.I = this.V[(this.opcode & 0x0F00) >> 8] * 5;
+                            this.I = this.V[x] * 5;
                             this.pc += 2;
                             break;
                         }
 
                         // 0xFX33 stores the binary representation of V[0xN] at addresses I, I + 1 and I + 2
                         case 0x0033: {
-                            this.memory[this.I] = this.V[(this.opcode & 0x0F00) >> 8] / 100;
-                            this.memory[this.I + 1] = (this.V[(this.opcode & 0x0F00) >> 8] / 10) % 10;
-                            this.memory[this.I + 2] = (this.V[(this.opcode & 0x0F00) >> 8] % 100) & 10;
+                            this.memory[this.I] = this.V[x] / 100;
+                            this.memory[this.I + 1] = (this.V[x] / 10) % 10;
+                            this.memory[this.I + 2] = (this.V[x] % 100) & 10;
                             this.pc += 2;
                             break;
                         }
 
                         // 0xFX55
                         case 0x0055: {
-                            for(int i = 0; i <= ((this.opcode & 0x0F00) >> 8); i++) {
+                            for(int i = 0; i <= (x); i++) {
                                 this.memory[I + i] = this.V[0 + i];
                             }
                             this.pc += 2;
@@ -464,7 +471,7 @@ class Chip8 {
 
                         // 0xFX65
                         case 0x0065: {
-                            for(int i = 0; i <= ((this.opcode & 0x0F00) >> 8); i++) {
+                            for(int i = 0; i <= (x); i++) {
                                 this.V[0 + i] = this.memory[I + i];
                             }
                             this.pc += 2;
